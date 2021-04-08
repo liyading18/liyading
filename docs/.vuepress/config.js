bf25ce1216cde3f367aa7a1357a01aab58b26b53
liyading <liyading@rtmap.com>
2021-04-08 16:06:55 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: 'reco',
+    title: 'Yours Blog',
+    description: '我要带你飞 ',
+    dest: './dist',
+    // port: '7777',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: [],
+        sidebar: {},
+    },
+}  
