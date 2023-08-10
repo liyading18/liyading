e3b4ac037a0ddee5481c097480ce2e0cebb722dd
liyading <liyading@lyzdfintech.com>
2023-08-10 12:08:38 +0800

    feat

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd dist
+cd ./docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.yourwebsite.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:liyading18/liyading18.github.io.git master
+git push -f https://gitee.com/liyading/liyading.git master
 # 推到博客仓库2备份代码
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
